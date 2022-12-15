fcc8cf904b92e67521069395d857acadd8ecda0d
Istiak Ahamed <istiakasib89@gmail.com>
2022-12-15 19:01:00 +0600

    Fundamental 1-25

---
+++ script.js
@@ -0,0 +1,330 @@
+/*let js='Amazing';
+if(js==='Amazing') alert("Javacript is a fun");
+console.log(72+12+32+45-56)
+console.log('jonas')
+let firstName= "Istiak";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let job1='Programmer';
+let job2 ='Teacher';
+
+console.log(job1)
+let namew ='Asif'
+
+// Variable Name convention  Comment
+
+hkgghg
+
+
+// console.log(true)
+
+console.log(typeof false);
+
+console.log(typeof 23);
+ console.log(typeof 'Istiak');
+
+let istiakIsaBadBoy =true;
+console.log(istiakIsaBadBoy);
+console.log(typeof istiakIsaBadBoy);
+istiakIsaBadBoy ='YES!';
+console.log(typeof istiakIsaBadBoy);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year=1998;
+console.log(typeof year);
+
+year= 'Anan';
+console.log(typeof year);
+
+year = true;
+console.log(typeof year);
+console.log(typeof null);
+*/
+
+/*let age = 30;
+age =31;
+
+const birthYear= 1999;
+//birthYear =1999;
+//const birth;
+// math operator
+var job ='youarebadboy';
+job = 'goodboy';
+
+lname ="Istiak_Ahamed";
+console.log(lname);*/
+
+/*const now=2023; 
+const ageistiak = now-2000;
+const ageritu = now - 2002;
+console.log(ageistiak,ageritu);
+console.log(ageistiak/2,ageistiak*2,2**4);
+// 2**4 means @ to the power 4
+
+const firstname='Istiak', lastname= 'Ritu';
+console.log(firstname+lastname);
+console.log(firstname+'     '+lastname);
+//assignment operator
+let x = 3+5;
+x += 10;
+x **=2;
+x++;
+console.log(x);
+// comparison operators
+console.log(ageistiak>ageritu);
+//>,<,>=,<=
+console.log(ageritu>=18);
+console.log(now-2000>now-2002);*/
+
+/*const now=2023; 
+const ageistiak = now-2000;
+const ageritu = now - 2002;
+console.log(now-2000>now-2002);//Math operator excute before than comparision operator
+const averageAge =(ageistiak+ageritu)/2
+console.log(ageistiak,ageritu,averageAge)
+*/
+
+
+//challenge=====1
+
+
+/*let marksmass=78;
+let marksheight=1.69;
+let johnmass=92;
+let johnheight=1.95;*//*
+
+let marksmass=95;
+let marksheight=1.88;
+let johnmass=85;
+let johnheight=1.76;
+
+let markbmi= marksmass/(marksheight**2);
+console.log(markbmi);
+
+let johnbmi= johnmass/(johnheight**2);
+console.log(johnbmi);
+
+console.log(johnbmi>markbmi);*/
+
+
+/*const firstName ='Istiak';
+const job='programmer';
+const birthyear=2002;
+const cyear=2022;
+const istiak="I'm " + firstName +', a ' + ( cyear-birthyear ) + ' years old ' + job + '!';
+console.log(istiak);
+
+const istiaknew = `I'm ${firstName}, a ${cyear-birthyear} years old ${job}!`;
+
+console.log(istiaknew);
+console.log(`ajgdj uhusid syausy`);
+console.log(' String with \n\ multiple \n\ lines');
+console.log(`String 
+with
+line`);*/
+
+/*const age = 19;
+const isoldenough = age>=29;
+if(isoldenough) {
+    console.log("Ritu can drive 🚗");
+}
+else
+{
+    const yearsLeft=29-age ;
+    console.log(`Ritu is too young. wait another ${yearsLeft} years :`);
+}
+
+let century;
+const birthyear = 2001;
+if(birthyear<=2000)
+{
+    century=20;
+}
+else{
+    century=21;
+}
+console.log(century);*/
+
+
+// let marksmass=95;
+// let marksheight=1.88;
+// let johnmass=85;
+// let johnheight=1.76;
+
+// let markbmi= marksmass/(marksheight**2);
+
+
+// let johnbmi= johnmass/(johnheight**2);
+// console.log(markbmi, johnbmi);
+
+// if (markbmi>johnbmi){
+//     console.log("Mark's bmi is higher than john's bmi");
+// }
+// else
+// {
+//     console.log("John's bmi is higher than marks bmi");
+
+// }
+// if (markbmi>johnbmi){
+//     console.log(`Mark's bmi is ${markbmi} higher than john's bmi ${johnbmi}`);
+// }
+// else
+// {
+//     console.log(`John's bmi ${johnbmi} is higher than marks bmi  ${markbmi}`);
+
+// }
+
+// const inputYear= '1991';
+// console.log(Number(inputYear),inputYear);
+// console.log(Number(inputYear)+18);
+// console.log(Number('Istiak'));
+// console.log(String(23),23);
+
+// console.log('I am' + 23 + 'years old');
+// console.log('23' + '10' - 3);
+// console.log('23' - '10' - 3);
+// console.log('23' / '3');
+// console.log('23' + 3);
+
+// let n= '1' + 1;
+// n= n-1;
+// console.log(n);
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('istiak'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+// const money=100;
+// if(money){
+//     console.log("sob khoroc koiro na");
+// }
+// else{
+//     console.log("ekta chakri khujo");
+// }
+// let height=0;
+// if(height){
+//     console.log('heght is defined');
+
+// }
+// else{
+//     console.log('is not defined');
+// }
+
+
+// const age='18';
+// if(age===18) console.log('an adult: (strict)');
+// if(age==18)console.log('become an adult (loose)');
+// const favourite =Number(prompt("What is your favourite number"));
+// console.log(favourite);
+// console.log(typeof favourite);
+
+// if (favourite===23){
+//     console.log('Cool! 23 is an amazing number!');
+// }
+// if (favourite===7){
+//     console.log('Cool! 7 is an amazing number!');
+// }
+// else{
+//     console.log('number is not 23 or 7')
+// }
+// if(favourite !==23) console.log('Why not 23');
+
+
+// boolean logic
+
+const hasDriverLicense= true;//a
+const hasGoodVision= true;//b
+// console.log(hasDriverLicense&&hasGoodVision);
+// console.log(hasDriverLicense||hasGoodVision);
+// console.log(!hasDriverLicense);
+// const shouldDrive= hasDriverLicense&&hasGoodVision;
+// if(shouldDrive){
+//     console.log('Ritu moni can drive');
+// }
+// else{
+//     console.log('Cant drive');
+// }
+
+// const istired= false;
+// if(hasDriverLicense && hasGoodVision && !istired){
+//     console.log('Ritu moni can drive');
+// }
+// else{
+//     console.log('Cant drive');
+// }
+
+//challenged 3
+
+// const dolphins1=97;
+// const dolphins2=112;
+// const dolphins3=101;
+// let average1;
+// average1=(dolphins1+dolphins2+dolphins3)/3;
+// console.log(average1);
+// const average2=(109+95+123)/3;
+// console.log(average2);
+// if(average1>average2 && average1>=100){
+//     console.log('Dolphin is the boss🦸🦸🦸🦸🦸🦸🦸‍♂️');
+// }
+// else if(average2>average1){
+//     console.log('Koalas is the hero🦸‍♂️🦸‍♂️🦸‍♂️');
+
+// }
+// else if(average1===average1)
+// {
+//     console.log('Both are win');
+// }
+// else{
+//     console.log('No one win');
+// }
+
+
+// const day='wednesday';
+
+// switch(day){
+//     case 'monday':
+//         console.log("I will Love you");
+//         break;
+//         case 'saturday':
+//             console.log("marry you");
+//             break;  
+//             case 'sunday':
+//                 console.log("Read book");
+//                 break ;
+//                 default:
+//                     console.log(" NOT VALID")            
+// }
+
+
+// teranery operator
+
+// const age=23
+// age>=18?console.log('I like roast'):
+// console.log('I like water');
+
+// const drink= age>=18? 'wine':'water';
+// console.log(drink)
+
+// let drink2;
+// if(age>=25){
+//     drink2='wine';
+// }
+// else{
+//     drink2='water';
+// }
+// console.log(drink2);
+// console.log(`I like to drink ${age>=18? 'wine':'water'}`);
+
+
+//  Coding challange 4
+
+// const bill=275;
+// const tip= bill<=300 && bill>=50? bill*(15/100):bill*(20/100);
+// console.log(`The bill was ${bill}, the tip was ${tip}, The total was${bill + tip}`);
